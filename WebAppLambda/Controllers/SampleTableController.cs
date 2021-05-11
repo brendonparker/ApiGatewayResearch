8ab7bb7505f09d87d94cc9c40f42f77885e3581a
Brendon Parker <bparker@icsfl.com>
2021-05-11 08:57:49 -0400

    Sample using Partiql

--- WebAppLambda/Controllers/SampleTableController.cs
+++ WebAppLambda/Controllers/SampleTableController.cs
@@ -2,7 +2,6 @@
 using Amazon.DynamoDBv2.DataModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,13 +25,17 @@ namespace WebAppLambda.Controllers
         }
 
         [HttpGet]
-        public async Task<List<ShipTestTable>> Get()
+        public async Task<List<ShipTestTable>> Get(bool delay = false)
         {
             var context = new DynamoDBContext(_dynamoDbClient);
-            var recorder = new Amazon.XRay.Recorder.Core.AWSXRayRecorder();
-            recorder.BeginSubsegment("Waiting 200ms");
-            await Task.Delay(200);
-            recorder.EndSubsegment();
+
+            if (delay)
+            {
+                var recorder = new Amazon.XRay.Recorder.Core.AWSXRayRecorder();
+                recorder.BeginSubsegment("Waiting 200ms");
+                await Task.Delay(200);
+                recorder.EndSubsegment();
+            }
 
             var asyncSearch = context.ScanAsync<ShipTestTable>(Enumerable.Empty<ScanCondition>());
 
@@ -40,13 +43,39 @@ namespace WebAppLambda.Controllers
 
             return results;
         }
+
+        [HttpGet("partiql/{id}")]
+        public async Task<List<ShipTestTable>> Get(string id)
+        {
+            var res = await _dynamoDbClient.ExecuteStatementAsync(new Amazon.DynamoDBv2.Model.ExecuteStatementRequest
+            {
+                Statement = "SELECT * FROM SampleTable WHERE PartitionKey = ?",
+                Parameters = new List<Amazon.DynamoDBv2.Model.AttributeValue>
+                {
+                    new Amazon.DynamoDBv2.Model.AttributeValue
+                    {
+                        S = id
+                    }
+                }
+            });
+
+            return res.Items.ConvertAll(x =>
+            {
+                var doc = Amazon.DynamoDBv2.DocumentModel.Document.FromAttributeMap(x);
+                return new ShipTestTable
+                {
+                    PartitionKey = doc[nameof(ShipTestTable.PartitionKey)],
+                    SortKey = doc[nameof(ShipTestTable.SortKey)],
+                };
+            });
+        }
     }
 
     [DynamoDBTable("SampleTable")]
     public class ShipTestTable
     {
         [DynamoDBHashKey("PartitionKey")]
-        public string ParitionKey { get; set; }
+        public string PartitionKey { get; set; }
 
         [DynamoDBRangeKey("SortKey")]
         public string SortKey { get; set; }
